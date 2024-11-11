0e37fbe6f5b1411b650afcac4e00f5edff6d480b
MenassieErmias <ermiasmenase@gmail.com>
2024-11-11 12:26:12 +0300

    Welcome component added and styled: Welcome.jsx

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -2,7 +2,16 @@
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <section className="welcome">
+      <div className="welcome-container">
+        <h1 className="welcome-heading">
+          Welcome To Yamato Engineering!
+        </h1>
+        <p className="welcome-desc">
+          At Yamato Engineering plc, we take pride in offering a comprehensive range of services tailored to meet the specific needs of our clients in the fields of Control Boards, Solar Power, and SCADA Water Distribution Systems. With our expertise and dedication to innovation, we strive to provide sustainable, efficient, and reliable solutions that drive success for businesses and industries worldwide.
+        </p>
+      </div>
+    </section>
   )
 }
 
