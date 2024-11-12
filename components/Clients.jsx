21561db8f756178038c9b9b380208df6fa9fcdc9
MenassieErmias <ermiasmenase@gmail.com>
2024-11-12 10:28:28 +0300

    clients component adn client card added: Clients.jsx, client and client card styled: home.css

--- components/Clients.jsx
+++ components/Clients.jsx
@@ -1,7 +1,43 @@
-
+import '@styles/home.css'
 const Clients = () => {
+  const ClientCard = ({ name }) => {
+    return (
+      <div className="client-card">
+        <div className="client-card-container">
+          <h3 className="client-card-heading">
+            {name}
+          </h3>
+          <hr />
+          <button className="client-card-btn">
+            Learn more
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  const clients = [
+    {
+      name: 'John Smith',
+    },
+    {
+      name: 'Helene Smith',
+    }
+  ]
   return (
-    <div>Clients</div>
+    <section className="clients">
+      <hr />
+      <h2 className="clients-heading">
+        Recent Clients
+      </h2>
+      <div className="clients-container">
+        {
+          clients.map((client, index) => (
+            <ClientCard name={client.name} key={index} />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
