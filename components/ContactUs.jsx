2f3506f52917d3851ba2498bd1e61b5f13be8810
MenassieErmias <ermiasmenase@gmail.com>
2024-11-12 10:58:42 +0300

    contact form added and styled: ContactUs.jsx and home.css

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -3,10 +3,31 @@ import Form from 'next/form'
 const ContactUs = () => {
   return (
     <section className="contact-us">
-      <div className="contact-us container">
-        <Form className="contact-us-form">
-          <input className='contact-form-input' name="Name" />
-          <button type="submit">
+      <hr />
+      <h2 className="contact-us-heading">
+        Send us a message.
+      </h2>
+
+      <div className="contact-us-container">
+        <Form className="contact-form">
+          <input
+            className='contact-form-input'
+            name="Name"
+            placeholder="Name"
+          />
+          <input
+            className='contact-form-input'
+            type="email"
+            name="email"
+            id="email"
+            placeholder='E-mail'
+          />
+          <input
+            className='contact-form-input' type="tel" name="phone" id="phone" placeholder='Phone Number' />
+          <textarea
+            className='contact-form-textarea'
+            name="message" id="message" placeholder='Message ...'></textarea>
+          <button className='contact-form-btn' type="submit">
             Send Message
           </button>
         </Form>
