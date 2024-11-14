d0158b40d4da173d7bddd2c1e0459cf2a7b354e9
MenassieErmias <ermiasmenase@gmail.com>
2024-11-14 13:47:28 +0300

    Services content added in an array, next step prepare cards: app/services/page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,10 +1,39 @@
 
 
 const Services = () => {
+
+  const services = [
+    {
+      imageURL: '',
+      title: 'Innovative and Customizable Solutions',
+      desc: 'We are committed to pushing the boundaries of technology and innovation. Our solutions are highly customizable, allowing us to tailor our services to meet the specific requirements of each client and project.'
+    },
+    {
+      imageURL: '',
+      title: 'Quality Assurance',
+      desc: 'Quality is at the core of everything we do. From design to implementation, we adhere to stringent quality assurance measures to ensure that our solutions are reliable, durable, and performance-oriented.'
+    },
+    {
+      imageURL: '',
+      title: 'Expert Team',
+      desc: 'Our team comprises skilled professionals with extensive experience in the respective fields of control boards, solar power, and SCADA water distribution systems. They are dedicated to delivering top-notch solutions that exceed expectations.'
+    },
+    {
+      imageURL: '',
+      title: 'Customer-Centric Approach',
+      desc: 'At Yamato Engineering plc, our clients are our top priority. We foster strong relationships with our clients and work closely with them to understand their needs, providing exceptional service and support at every stage of the project.'
+    }
+  ]
+
   return (
-    <main className="services">
-      Services
-    </main>
+    <section className="services">
+      <div className="services-container">
+        <h2 className="services-heading">
+          WHY CHOOSE YAMATO ENGINEERING PLC?<br /> We offer,
+        </h2>
+
+      </div>
+    </section>
   )
 }
 
