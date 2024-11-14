d5cdaa50c0d2b8297ede6b2b4c2fecad6d632ce5
MenassieErmias <ermiasmenase@gmail.com>
2024-11-14 14:20:10 +0300

    Images and ServiceCard Component added, ServiceCard componet found inside app/services/page.jsx since its not really reuseable: public/assests/images/ and app/services/page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,29 +1,51 @@
 
-
+import Image from "next/image";
 const Services = () => {
 
   const services = [
     {
-      imageURL: '',
+      imageURL: '/assets/images/innovative.jpg',
       title: 'Innovative and Customizable Solutions',
       desc: 'We are committed to pushing the boundaries of technology and innovation. Our solutions are highly customizable, allowing us to tailor our services to meet the specific requirements of each client and project.'
     },
     {
-      imageURL: '',
+      imageURL: '/assets/images/quality-assurance.jpg',
       title: 'Quality Assurance',
       desc: 'Quality is at the core of everything we do. From design to implementation, we adhere to stringent quality assurance measures to ensure that our solutions are reliable, durable, and performance-oriented.'
     },
     {
-      imageURL: '',
+      imageURL: '/assets/images/expert-team.jpg',
       title: 'Expert Team',
       desc: 'Our team comprises skilled professionals with extensive experience in the respective fields of control boards, solar power, and SCADA water distribution systems. They are dedicated to delivering top-notch solutions that exceed expectations.'
     },
     {
-      imageURL: '',
+      imageURL: '/assets/images/customer-centric.jpg',
       title: 'Customer-Centric Approach',
       desc: 'At Yamato Engineering plc, our clients are our top priority. We foster strong relationships with our clients and work closely with them to understand their needs, providing exceptional service and support at every stage of the project.'
     }
   ]
+
+  const ServicesCard = ({ imageURL, title, desc }) => {
+    return (
+      <div className="service-card">
+        <div className="service-card-container">
+          <Image
+            className="service-card-image"
+            src={imageURL}
+            alt={title}
+            height={200}
+            width={100}
+          />
+          <h4 className="sercice-card-title">
+            {title}
+          </h4>
+          <p className="service-card-desc">
+            {desc}
+          </p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <section className="services">
@@ -31,7 +53,11 @@ const Services = () => {
         <h2 className="services-heading">
           WHY CHOOSE YAMATO ENGINEERING PLC?<br /> We offer,
         </h2>
-
+        {
+          services.map((service, index) => (
+            <ServicesCard key={index} imageURL={service.imageURL} title={service.title} desc={services.desc} />
+          ))
+        }
       </div>
     </section>
   )
